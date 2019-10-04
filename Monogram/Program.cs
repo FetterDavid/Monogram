7a32e43f5185234aa045ce692197d4ce0be28890
FetterDavid <56163065+FetterDavid@users.noreply.github.com>
2019-10-04 15:59:08 +0200

    Initial commit

---
+++ Monogram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] duplaBetuk = new string[] {"Sz","Zs","Ty","Ly","Ny","Gy","Cs","Dz","Dzs","Dr."};
+            int i;
+
+            Console.Write("Név: ");
+            string nev = Console.ReadLine().ToUpper();
+            string[] nevek = nev.Split(' ');
+            Console.Write("Monogram: ");
+
+            for (int y = 0; y < nevek.Length; y++)
+            {
+                for (i = 0; i < duplaBetuk.Length; i++)
+                {
+                    if (nevek[y].StartsWith(duplaBetuk[i].ToUpper()))
+                    {
+                        Console.Write(duplaBetuk[i]);
+                        break;
+                    }
+                }
+                if (i == duplaBetuk.Length && nevek[y].Length>0) Console.Write(nevek[y][0]);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
